ed3a45338d2c2a05a1e4a801ecc4519ad6edb1a7
lenarsaitov <lenarsaitov1@yandex.ru>
2023-02-05 00:51:03 +0300

    update setup lib configuration

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 
+with open("README.md") as file:
+    read_me_description = file.read()
+
+
 setup(
     name='cianparser',
     version='0.4.8',
@@ -9,9 +13,14 @@ setup(
     author_email='lenarsaitov1@yandex.ru',
     license='MIT',
     packages=['cianparser'],
-    long_description=['file: README.md'],
-    long_description_content_type=['text/x-rst; charset=UTF-8'],
-    classifiers=[],
+    long_description=read_me_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     keywords='python parser requests cloudscraper beautifulsoup cian realstate',
     install_requires=['cloudscraper', 'beautifulsoup4', 'transliterate', 'lxml', 'datetime'],
+    python_requires='>=3.9',
 )

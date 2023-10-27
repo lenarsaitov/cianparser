290f1e96964f9633e652c027047de4791d8bc57c
lenarsaitov <l.saitov@4payments.com>
2023-10-27 21:17:52 +0300

    updating location-related data

--- cianparser/definer_cities_id.py
+++ cianparser/definer_cities_id.py
@@ -25,33 +25,47 @@ class Client:
         self.start_location_id = start_location_id
         self.end_location_id = end_location_id
 
-    def define_city(self, html, location):
+    def define_city(self, html, location_id: int):
         soup = BeautifulSoup(html, 'html.parser')
         offers = soup.select("div[data-name='HeaderDefault']")
 
+        if len(offers) == 0:
+            print("_" + "  " + "***")
+            return self.cities
+
         title = offers[0].text
-        city = title[:title.find('Аренда')].split()[-1]
-        if city == "России":
-            print(location)
+        city = title.lower()[title.lower().find("снять квартиру в ") + len("снять квартиру в "):title.lower().find(
+            " на длительный срок")]
+
+        if ("в России" in title or "АрендаСнять" not in title or
+                ("области" in city or "крае" in city or "республике" in city or
+                 "округе" in city or "россии" in city or
+                 "кабардино" in city or "карачаево" in city or
+                 "дагестан" in city or "осетии" in city or
+                 "ненецком ао" in city or "ямало-ненецком ао" in city or
+                 "чукотском ао" in city or "ханты-мансийском ао" in city or
+                 "чувашии" in city)
+        ):
+            print("_" + "  " + str(location_id))
             return self.cities
 
         morph = pymorphy2.MorphAnalyzer()
         city = morph.parse(city)[0].normal_form.title()
-        print(city + " " + str(location))
+        print(city + " " + str(location_id))
 
         if city not in self.cities_set:
             self.cities_set.add(city)
-            self.cities.append((city, location))
+            self.cities.append((city, location_id))
             self.save_results()
 
         return self.cities
 
     def define_all_cities(self):
-        for location in range(self.start_location_id, self.end_location_id):
-            path = f'https://www.cian.ru/cat.php?deal_type=rent&engine_version=2&offer_type=flat&p=1&region={location}&type=4'
+        for location_id in range(self.start_location_id, self.end_location_id):
+            path = f'https://www.cian.ru/cat.php?deal_type=rent&engine_version=2&offer_type=flat&p=1&region={location_id}&type=4'
             response = requests.get(path)
             html = response.text
-            self.define_city(html, location)
+            self.define_city(html, location_id)
 
         self.cities = sorted(self.cities, key=lambda x: x[0])
 

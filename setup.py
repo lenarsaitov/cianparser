1a9178250f09b1096393a92850643a420afdbe54
lenarsaitov <l.saitov@4payments.com>
2024-01-09 01:45:34 +0300

    update version to 0.4.30

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf8") as file:
 
 setup(
     name='cianparser',
-    version='0.4.29',
+    version='0.4.30',
     description='Parser information from Cian website',
     url='https://github.com/lenarsaitov/cianparser',
     author='Lenar Saitov',

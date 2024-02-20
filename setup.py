d7e7167fdb1997d92a9db3eb055101eddd10d95f
lenarsaitov <l.saitov@4payments.com>
2024-02-20 20:04:50 +0300

    fix (add init files on packages)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf8") as file:
 
 setup(
     name='cianparser',
-    version='1.0.2',
+    version='1.0.3',
     description='Parser information from Cian website',
     url='https://github.com/lenarsaitov/cianparser',
     author='Lenar Saitov',
